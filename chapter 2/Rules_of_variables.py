fcede76c45218e22606d68b8aca3485b59274a61
rohansng <buisnessorg3@gmail.com>
2024-12-10 23:10:13 +0530

    Added chapter 2

---
+++ chapter 2/Rules_of_variables.py
@@ -0,0 +1,17 @@
+aaa = 435
+harry = 34
+_sameer = 45
+S_am=13
+samerr = 34
+one8 = 24
+# @sameer = 56 # Invalid due to @ symbol
+# s@meer = 23 # Invalid due to @ symbol
+# Sam rohan = 21 #Invalid due to white space 
+
+'''RULES FOR CHOOSING AN IDENTIFIER
+1. A variable name can contain alphabets, digits, and underscores.
+2. A variable name can only start with an alphabet and underscores.
+3. A variable name can’t start with a digit.
+4. No while space is allowed to be used inside a variable name.'''
+
+print (aaa+harry)
